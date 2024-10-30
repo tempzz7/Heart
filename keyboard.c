305e4d5fa5f0eaab157c87dd7d8789f070046493
tempz <tcq@cesar.school>
2024-10-30 12:25:32 -0300

    Insertion

--- keyboard.c
+++ keyboard.c
@@ -1,22 +1,22 @@
 /**
- * keyboard.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
+
+keyboard.c
+Created on Aug, 23th 2023
+Author: Tiago Barros
+Based on "From C to C++ course - 2002"*/
 
 #include <termios.h>
 #include <unistd.h>
+#include <stdio.h>  // Adicionado para usar perror
 
 #include "keyboard.h"
 
 static struct termios initialSettings, newSettings;
-static int peekCharacter;
-
+static int peekCharacter = -1;
 
 void keyboardInit()
 {
-    tcgetattr(0,&initialSettings);
+    tcgetattr(0, &initialSettings);
     newSettings = initialSettings;
     newSettings.c_lflag &= ~ICANON;
     newSettings.c_lflag &= ~ECHO;
@@ -36,33 +36,43 @@ int keyhit()
     unsigned char ch;
     int nread;
 
-    if (peekCharacter != -1) return 1;
-    
-    newSettings.c_cc[VMIN]=0;
+    if (peekCharacter != -1)
+        return 1;
+
+    newSettings.c_cc[VMIN] = 0;
     tcsetattr(0, TCSANOW, &newSettings);
-    nread = read(0,&ch,1);
-    newSettings.c_cc[VMIN]=1;
+    nread = read(0, &ch, 1);
+    newSettings.c_cc[VMIN] = 1;
     tcsetattr(0, TCSANOW, &newSettings);
-    
-    if(nread == 1) 
+
+    if (nread == 1)
     {
         peekCharacter = ch;
         return 1;
     }
-    
+
     return 0;
 }
 
 int readch()
 {
     char ch;
+    int result;
 
-    if(peekCharacter != -1)
+    if (peekCharacter != -1)
     {
         ch = peekCharacter;
         peekCharacter = -1;
         return ch;
     }
-    read(0,&ch,1);
+
+    result = read(0, &ch, 1);
+    if (result == -1)
+    {
+        // Lidar com erro de leitura
+        perror("Erro na leitura de caractere");
+        return -1; // Ou algum outro valor indicativo de erro
+    }
+
     return ch;
 }
